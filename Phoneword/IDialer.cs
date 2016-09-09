e6dce62d4e546464d59f935d6db5540bc1ea5461
klg73x <klg73@outlook.com>
2016-09-09 10:23:14 -0400

    Initial check-in of module Phoneword

---
+++ Phoneword/IDialer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Phoneword
+{
+	public interface IDialer
+	{
+		Task<bool> DialAsync(string number);
+	}
+}
+
---
+++ Phoneword/MainPage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+	public class MainPage : ContentPage
+	{
+		Entry txtPhoneNumber = new Entry();
+		Button bttnTranslate = new Button();
+		Button bttnCall = new Button();
+		string translatedNumber = "";
+
+		public MainPage()
+		{
+			StackLayout mainStack = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Thickness(20, Device.OnPlatform(40, 20, 20), 20, 20),
+				Spacing = 15
+			};
+			Label lblTitle = new Label
+			{
+				Text = "Enter a Phoneword:"
+			};
+
+			txtPhoneNumber = new Entry
+			{
+				Text = "1-855-XAMARIN"
+			};
+
+			bttnTranslate = new Button
+			{
+				Text = "Translate"
+			};
+			bttnTranslate.Clicked += BttnTranslate_Clicked;
+
+			bttnCall = new Button
+			{
+				Text = "Call",
+				IsEnabled = false
+			};
+			bttnCall.Clicked += bttnCall_Clicked;
+
+			mainStack.Children.Add(lblTitle);
+			mainStack.Children.Add(txtPhoneNumber);
+			mainStack.Children.Add(bttnTranslate);
+			mainStack.Children.Add(bttnCall);
+			this.Content = mainStack;
+		}
+
+		void BttnTranslate_Clicked(object sender, EventArgs e)
+		{
+			string phonenumber = txtPhoneNumber.Text;
+			translatedNumber = Core.PhonewordTranslator.ToNumber(phonenumber);
+			if (!string.IsNullOrEmpty(translatedNumber))
+			{
+				bttnCall.IsEnabled = true;
+				bttnCall.Text = "Call: " + translatedNumber;
+			}
+			else { bttnCall.IsEnabled = false; bttnCall.Text = "Call"; }
+		}
+
+		async void bttnCall_Clicked(object sender, EventArgs e)
+		{
+			string callmessage = "Would you like to call " + translatedNumber;
+			if (await DisplayAlert("Dial a Number", callmessage, "Yes", "No"))
+			{
+				//Dial the number
+				var dialer = DependencyService.Get<IDialer>();
+				if (dialer != null)
+				{
+					await dialer.DialAsync(translatedNumber);
+				}
+			}
+		}
+	}
+}
+
